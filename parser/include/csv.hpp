f19f89073f519c10129f2ed229ffae12c5aeb714
Andrey Turin <8150711@edu.miet.ru>
2018-12-09 20:51:57 +0300

    Лабораторная работа parser(csv)

--- parser/include/csv.hpp
+++ parser/include/csv.hpp
@@ -37,7 +37,7 @@ namespace types
     {
         //? Which kind of types should I use to describe the CSV type?
         //{
-        ... csv = ...
+        using csv = std::vector<std::vector<std::string>>;
         //}
     }
 }
@@ -51,15 +51,15 @@ namespace parser
         namespace x3 = boost::spirit::x3;
 
         //{ csv grammar
-        ... string = ...
-        ... cell = ...
-                 = ...
+        const auto string = x3::lexeme[+(~x3::char_(",\n")];
+        const auto cell = x3::rule<class row, std::string> {}
+                 = parser::quoted_string | string;
 
-        ... row = ...
-                = ...
+        const auto row = x3::rule<class row, std::vector<std::string>> {}
+                = cell % ',';
 
-        ... csv = ...
-                = ...
+        const auto csv = x3::rule<class csv, types::csv::csv> {}
+                = +(row);
         //}
     }
 }
@@ -69,10 +69,10 @@ namespace literals
     namespace csv
     {
         //{ declare ``_csv`` literal
-        ... _csv ..
+        types::csv::csv operator "" _csv(const char* str, size_t l)
         {
-
-        }
+            return parser::load_from_string<types::csv::csv>(std::string(str, l), parser::csv::csv);
+}
         //}
     }
 }
